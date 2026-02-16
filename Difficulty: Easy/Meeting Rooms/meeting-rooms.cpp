c502ef7025482fced95fdf5916656b967fae7884
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-02-16 09:53:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Code Here
+        int n=arr.size(),c=0;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n-1;i++){
+            if(arr[i+1][0]<arr[i][1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
