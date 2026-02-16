c5424dcecaa097b3984acf35f339cfa2f21cd496
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-02-16 12:12:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Queue Reversal/queue-reversal.cpp
+++ Difficulty: Easy/Queue Reversal/queue-reversal.cpp
@@ -1,16 +1,14 @@
 class Solution {
   public:
+    void solve(queue<int>&q){
+       if(q.empty()) return;
+       int frontele=q.front();
+       q.pop();
+       solve(q);
+       q.push(frontele);
+    }
     void reverseQueue(queue<int> &q) {
         // code here
-        // vector<int>res;
-        stack<int>st;
-        while(!q.empty()){
-            st.push(q.front());
-            q.pop();
-        }
-        while(!st.empty()){
-            q.push(st.top());
-            st.pop();
-        }
+        solve(q);
     }
 };
