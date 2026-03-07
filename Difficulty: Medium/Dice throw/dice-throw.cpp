aa637892ef5349bce686984e1444cd0c92e18f29
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-07 14:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dice throw/dice-throw.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int solve(int m,int n,int x,vector<vector<int>>&dp){
+        if(n==0 && x==0){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        if(x<0){
+            return 0;
+        }
+        if(dp[n][x]!=-1){
+            return dp[n][x];
+        }
+        int ans = 0;
+        for(int i=1;i<=m;i++){
+            ans+=solve(m,n-1,x-i,dp);
+        }
+        return dp[n][x]=ans;
+        
+    }
+    int noOfWays(int m, int n, int x) {
+        // code here
+        vector<vector<int>>dp(n+1,vector<int>(x+1,-1));
+        return solve(m,n,x,dp);
+        
+    }
+};
