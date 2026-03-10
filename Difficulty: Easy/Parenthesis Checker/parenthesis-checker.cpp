eab45220f99ce4ed7348ff701f86bd6819009d33
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-10 09:40:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -2,25 +2,24 @@ class Solution {
   public:
     bool isBalanced(string& s) {
         // code here
-        stack<char>st;
+        int top=-1;
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
+                s[++top]=s[i];
             }
-            else if(s[i]==')' || s[i]=='}' || s[i]==']'){
-                if(st.empty()){
+            else{
+                if(top==-1){
                     return false;
                 }
-                if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') ||
-                (s[i]=='}' && st.top()=='{')){
-                    st.pop();
+                else if((s[i]=='}' && s[top]=='{') || (s[i]==')' && s[top]=='(')
+                || (s[i]==']' && s[top]=='[')){
+                    top--;
                 }
                 else{
                     return false;
                 }
             }
         }
-        if(st.empty()) return true;
-        return false;
+        return top==-1;
     }
 };
